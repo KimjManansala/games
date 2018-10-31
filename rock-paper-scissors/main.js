cd92e10c90576a1136c00f530b9d307a602dece6
KimjManansala <kimjmanansala@gmail.com>
2018-10-30 21:19:57 -0500

    rock paper scissors completed

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,66 +1,109 @@
-
-
-
-
-let gameResults = {
-    player1: ' ',
-    computer: ' '
+const content = document.getElementById('content');
+let theGame = {
+    player: '',
+    computer: ''
 }
 
-const content = document.getElementById('content');
-
-document.addEventListener('DOMContentLoaded', init)
-
+document.addEventListener('DOMContentLoaded', init(theGame))
+content.addEventListener('click', humanPlay)
 
 function init() {
-    console.info('Initializing Rock Paper Scissors!')
-    renderSkeletonGame()
-    //Add a function to the bubblinx
-
-
+    console.info("Game has initialized")
+    content.innerHTML = renderGame(theGame)
+    console.info('RenderGame function has initialized')
 
 }
 
+function checkWinner() {
+    if (theGame.player === 'Rock') {
+        if (theGame.computer === 'Rock') {
+            renderDraw()
+            return
+        }
+        if (theGame.computer === 'Paper'){
+            renderLose()
+            return
+        }
+        if (theGame.computer === 'Scissor'){
+            renderWin()
+            return
+        }
+    }
+    if (theGame.player === 'Paper') {
+        if (theGame.computer === 'Rock') {
+            renderWin()
+            return
+        }
+        if (theGame.computer === 'Paper'){
+            renderDraw()
+            return
+        }
+        if (theGame.computer === 'Scissor'){
+            renderLose()
+            return
+        }
+    }
+    if (theGame.player === 'Scissor') {
+        if (theGame.computer === 'Rock') {
+            renderLose()
+            return
+        }
+        if (theGame.computer === 'Paper'){
+            renderWin()
+            return
+        }
+        if (theGame.computer === 'Scissor'){
+            renderDraw()
+            return
+        }
+    }
+}
 
+function humanPlay(evt) {
+    computerPlay(theGame)
 
+    console.info('Human will make a play')
+    const target = evt.target
+    if (target.id === 'rock') {
+        console.info('Human chose Rock')
+        theGame.player = 'Rock'
 
+    } else if (target.id === 'paper') {
+        console.info('Human chose Paper')
+        theGame.player = 'Paper'
 
+    } else if (target.id === 'scissor') {
+        console.info('Human chose Scissor')
+        theGame.player = 'Scissor'
 
-function computerPlay(computerChoice, gameResults) {
-    //Checks if computer choise is valid
-    if (typeof computerChoice !== 'string') {
-        console.error('**ERROR** Computer choice should be a string')
+    }else{
         return
     }
-    if (computerChoice !== '' || computerChoice.toUpperCase !== 'ROCK' || computerChoice.toUpperCase !== 'PAPER' || computerChoice.toUpperCase !== 'SCISSOR') {
-        console.error(`**ERROR** Computer choice should be PAPER, SCISSORS, OR ROCK. Computer choice = ${computerChoice}`)
-        return
+    checkWinner()
+
+
+}
+
+
+function computerPlay() {
+    let x = Math.round(Math.random() * (2) + 1)
+    switch (x) {
+        case 1:
+            console.info('Computer will play Paper')
+            theGame.computer = 'Paper'
+            break
+        case 2:
+            console.info('Computer will play Scissor')
+            theGame.computer = 'Scissor'
+            break
+        case 3:
+            console.info('Computer will play Rock')
+            theGame.computer = 'Rock'
+            break
+        default:
+            console.error('X should be a number through not ' + x)
+            break
     }
-
-    gameResults.computer = computerChoice
-}
-
-
-
-
-
-
-function setComputerPlay(gameResults) {
-    let x = Math.floor((Math.random() * 3) + 1)
-    if (x === 1) {
-        gameResults.computer = 'ROCK'
-    } else if (x === 2) {
-        gameResults.computer = 'PAPER'
-    } else if (x === 3) {
-        gameResults.computer = 'SCISSOR'
-    } else {}
-
-    function checkWinner(gameResults) {
-        //CHECK THE WINNDER AND RENDERS THE WINNNER
-        renderResults(gameResults)
-
-    }
-
 }
 
 
@@ -69,49 +112,51 @@ function setComputerPlay(gameResults) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-function renderGame(gameResults) {
-    renderSkeletonGame()
-    //TODO ALL OTHER FUNCTIONS
-    renderResults(gameResults);
-
-
+//--------------------–––––––––––––––––––––––––––––––––––––––––––––––––––––----------–––––––
+//Rendering functions
+//--------------------–––––––––––––––––––––––––––––––––––––––––––––––––––––----------–––––––
+function renderWin() {
+    //Render winner
+    renderResults()
+    console.info('Human has won')
+    document.getElementById('result-text').innerHTML = 'YOU WIN'
 }
 
-function renderResults(results) {
-    let resultsRender = `<h3> You chose ${results.player1}!</h3>
-                        <h3>The computer chose ${results.computer}!</h3>`
-    document.getElementById('results').innerHTML = resultsRender
+function renderLose() {
+    //Render Loser
+    renderResults()
+    console.info('Computer has won')
+    document.getElementById('result-text').innerHTML = 'YOU LOSE'
+}
+
+function renderDraw() {
+    //Render Draw
+    renderResults()
+    console.info('Neither has won')
+    document.getElementById('result-text').innerHTML = 'IT IS A DRAW'
+}
+function renderResults (){
+    document.getElementById('human-play').innerHTML = `You played: <strong>${theGame.player}</strong>`
+    document.getElementById('computer-play').innerHTML = `The computer played: <strong>${theGame.computer}</strong>`
 }
 
 
 
-function renderSkeletonGame() {
-    let skeleton = `
+function renderGame() {
+
+    return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>Choose your <strong>weapon!!!</strong>:</h4>
-            <div class="w-50 text-center" id="weapon-choice">
-                <button class="btn btn-primary" id="rock-button">Rock</button>
-                <button class="btn btn-primary" id="paper-button">Paper</button>
-                <button class="btn btn-primary" id="scissors-button">Scissors</button>
+            <h4>Choose your weapon:</h4>
+            <div class="w-50 text-center">
+                <button class="btn btn-primary" id='rock'>Rock</button>
+                <button class="btn btn-primary" id='paper'>Paper</button>
+                <button class="btn btn-primary" id='scissor'>Scissors</button>
             </div>
-            <div class="d-flex justify-content-center" id="results">
-
-
+            <div class="d-flex justify-content-center">
+                <div class="m-5" id='human-play'>You played: </div>
+                <div class="m-5" id='computer-play'>The computer played: </div>
             </div>
-            <h1 class="text-center">You win!</h1>
+            <h1 class="text-center" id='result-text'></h1>
         </div>
     `
-    content.innerHTML = skeleton;
-
 }
